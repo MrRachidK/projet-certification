9c05e693bf8636dc06663b73c1e5064181895477
MrRachidK <rachid.karbiche.pro@gmail.com>
2022-06-08 09:39:54 +0200

    test

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from products_app import app
+import sys 
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from pokemon_app import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)

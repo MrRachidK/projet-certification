b7946781cabde05a0fcf88a02a85d22f10b38017
MrRachidK <rachid.karbiche.pro@gmail.com>
2022-08-05 18:25:49 +0200

    tests updated

--- tests/tests_unitaires/test_models.py
+++ tests/tests_unitaires/test_models.py
@@ -5,11 +5,12 @@ from pokemon_app.models import User
 ### Test de json
 
 def test_user_json(client):
-    User(last_name = "Mister", first_name = "T", email = "test@example.com", username = "MT", password = 'sha256$zU9nb9Fu6i2pLdmP$a1587105ab6efe3dd726cade3e95ab3ac039d58c0ecf40395871a0a071948c8f').save_to_db()
+    User(id = 1, last_name = "Mister", first_name = "T", email = "test@example.com", username = "MT", password = 'sha256$zU9nb9Fu6i2pLdmP$a1587105ab6efe3dd726cade3e95ab3ac039d58c0ecf40395871a0a071948c8f').save_to_db()
     # Get the user from the database
-    assert User.find_by_email("test@example.com").user_json() == {'last_name': 'Mister', 'first_name': 'T', 'email': 'test@example.com', 'username': 'MT'}
+    assert User.find_by_email("test@example.com").user_json() == {'id': 1, 'last_name': 'Mister', 'first_name': 'T', 'email': 'test@example.com', 'username': 'MT'}
 
 
 def test_find_by_email(client):
     User(last_name = "Mister", first_name = "T", email = "test@example.com", username = "MT", password = 'sha256$zU9nb9Fu6i2pLdmP$a1587105ab6efe3dd726cade3e95ab3ac039d58c0ecf40395871a0a071948c8f').save_to_db()
     assert User.find_by_email("test@example.com").username == "MT"
+
